7ac7c7af07493b419626cb0916ebbb8402c1e078
xuyapeng <53531436@qq.com>
2019-12-28 23:00:00 +0800

    修改目录

---
+++ src/views/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>个人技术展示</h1>
+    <p>正在更新，敬请期待。。。</p>
+
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+}
+</script>
